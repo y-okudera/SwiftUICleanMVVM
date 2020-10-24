c8476db32da02b5ad5b1dc0698d72fea11aeee20
okudera <appledev.yuoku@gmail.com>
2020-10-24 15:21:19 +0900

    Initial Commit

---
+++ NetworkCommunicator/NetworkCommunicatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCommunicatorApp.swift
+//  NetworkCommunicator
+//
+//  Created by okudera on 2020/10/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCommunicatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
